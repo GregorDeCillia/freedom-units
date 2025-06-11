7fb7ada41baa7fdb2ce9c10ed3937a896191aac8
Gregor de Cillia <de.cillia.gregor@gmail.com>
2025-06-11 15:24:08 +0200

    specify test names

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -4,6 +4,24 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 
 let convertHandler = new ConvertHandler();
 
-suite('Unit Tests', function(){
-
+suite('Unit Tests: convertHandler should correctly...', function () {
+    // parse numbers
+    test('read a whole number input.')
+    test('read a decimal number input.')
+    test('read a fractional input.')
+    test('read a fractional input with a decimal.')
+    test('return an error on a double-fraction (i.e. 3/2/3).')
+    test('default to a numerical input of 1 when no numerical input is provided.')
+    // parse units
+    test('read each valid input unit.')
+    test('return an error for an invalid input unit.')
+    test('return the correct return unit for each valid input unit.')
+    test('return the spelled-out string unit for each valid input unit.')
+    // convert
+    test('convert gal to L')
+    test('convert L to gal')
+    test('convert mi to km')
+    test('convert km to mi')
+    test('convert lbs to kg')
+    test('convert kg to lbs')
 });
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -5,6 +5,10 @@ const server = require('../server');
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
-
+suite('Functional Tests: GET /api/convert to convert...', function() {
+    test(' a valid input such as 10L')
+    test(' an invalid input such as 32g')
+    test(' an invalid number such as 3/7.2/4kg')
+    test(' an invalid number AND unit such as 3/7.2/4kilomegagram')
+    test(' with no number such as kg')
 });
